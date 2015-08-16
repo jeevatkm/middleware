6f3eec4e78d372a3e8d2d61761cb0b8d4955d95c
Jeevanandam M <jeeva@myjeeva.com>
2015-08-16 16:12:16 -0700

    refactoring done

--- doc.go
+++ doc.go
@@ -1,7 +1,7 @@
 /*
-Package handlers is a collection of handlers/HTTP middleware for use
-with Go's net/http package in other words any framework supporting http.Handler.
-	1. Minify using 'tdewolff/minify'
-	2. Options
+Package middleware is a collection of HTTP middleware/Handler function for use
+with Go's net/http package in other words any framework supporting http.Handler
+	1. Minify middleware using 'tdewolff/minify'
+	2. Options middleware
 */
-package handlers
+package middleware
